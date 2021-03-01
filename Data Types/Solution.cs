4a42786da58bd6e7e6bb2b2c87953a25dcde69df
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-03-01 23:10:54 +0300

    Day 1

---
+++ Data Types/Solution.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+    }
+}
